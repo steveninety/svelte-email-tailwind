1e84e486b4ae0752b0679310f83d33d48c5c6c1a
steveninety <admin@stevenpolak.me>
2024-02-29 19:25:28 +0100

    todo.md & script WIP

---
+++ TODO.md
@@ -0,0 +1,6 @@
+# Create emails on build time - only replace prop slots for props
+
+## Vite plugin: Building the emails
+1. Import and compile all email components (allow for folder specification).
+2. For each compiled JS component code, process tw classes.
+3. Write each manipulated component code to a file (allow for file location customization).
--- src/lib/buildEmails.ts
+++ src/lib/buildEmails.ts
@@ -5,10 +5,14 @@ import path from 'path'
 import { URL } from 'url';
 import { cleanCss, getMediaQueryCss, makeCssMap } from '$lib/renderTailwind';
 import { tailwindToCSS, type TailwindConfig } from 'tw-to-css'
+import type { ComponentProps } from 'svelte';
+import type WelcomeTailwindProps from '$lib/emails/welcome-tailwind.svelte'
 
 const __filename = new URL('', import.meta.url).pathname;
 const __dirname = new URL('.', import.meta.url).pathname;
 const pathToComponent = path.join(__dirname, './src/lib/emails')
+const pathToSsrCode = path.join(__dirname, 'welcome-tailwind.js')
+let code = compile(content, { generate: 'ssr' }).js.code
 
 const tailwindConfig: TailwindConfig = {
   theme: {
@@ -24,6 +28,18 @@ const tailwindConfig: TailwindConfig = {
   }
 }
 
+// If Tailwind was used, proceed to process the Tailwind classes
+const { twi } = tailwindToCSS({ config: tailwindConfig })
+
+// convert tailwind classes to css
+const twCss = twi(code, {
+  merge: false,
+  ignoreMediaQueries: false
+})
+
+// further process the tailwind css
+const cleanTwCss = cleanCss(twCss)
+
 // OBJECTIVE: transform tailwind classes to inline styles inside the compiled js ssr component file.
 // 1. find every .$$render( $$result, { alt: "Svelte logo", class: "my-0 mr-auto" },
 // 2. JSON.parse the obj after "$$result,"
@@ -31,69 +47,101 @@ const tailwindConfig: TailwindConfig = {
 // 4. stringify the obj and replace the old stringified obj
 // 5. Now these JS files can be imported as ssr components that can be .render()-ed
 
-const pathToSsrCode = path.join(__dirname, 'welcome-tailwind.js')
-let code = compile(content, { generate: 'ssr' }).js.code
-// If Tailwind was used, proceed to process the Tailwind classes
-const { twi } = tailwindToCSS({ config: tailwindConfig })
+export async function buildTailwind() {
 
-// convert tailwind classes to css
-const tailwindCss = twi(code, {
-  merge: false,
-  ignoreMediaQueries: false
-})
+  // 1.
+  const regexStart = /\$\$result,\s*{/g
+  let matchStart
 
-// further process the tailwind css
-const cleanTailwindCss = cleanCss(tailwindCss)
-const headStyle = `<style>${getMediaQueryCss(cleanTailwindCss)}</style>`
-// Perform checks so that responsive styles can be processed
-const hasResponsiveStyles = /@media[^{]+\{(?<content>[\s\S]+?)\}\s*\}/gm.test(headStyle)
+  const matchIndices = Array.from(code.matchAll(regexStart), (m) => {
+    return { start: m.index, end: m.index + m[0].length }
+  })
 
-// 1.
-const regexStart = /\$\$result,\s*{/g
-let matchStart
+  console.log(matchIndices)
 
-while ((matchStart = regexStart.exec(code)) !== null) {
-  const stringStartingAtMatch = code.substring(regexStart.lastIndex - 1, code.length)
-  const indexStart = code.length - stringStartingAtMatch.length
-  const regexEnd = /\}/g
-  let matchPre = stringStartingAtMatch
+  while ((matchStart = regexStart.exec(code)) !== null) {
+    const stringStartingAtMatch = code.substring(regexStart.lastIndex - 1, code.length)
+    console.log(regexStart.lastIndex)
+    const indexStart = code.length - stringStartingAtMatch.length
+    // match up to first closing bracket, e.g. '{ name: { first: { callMe: 'steven' }'
+    let matchPre = stringStartingAtMatch.substring(0, stringStartingAtMatch.indexOf('}') + 1) // e.g. { class: "text-[16px]" }
+    // count all '{'
+    const bracketOpenCount = matchPre.match(/\{/g).length
+    // find all '}' 
+    const bracketCloseCount = stringStartingAtMatch.matchAll(/\}/g)
+
+    // if style prop was a nested obj
+    if (bracketCloseCount > 2) {
+      throw new Error('You have passed in a nested object as a style prop. Email component style props cannot be nested.')
+    }
+
     // for every additional '{' inbetween, find the next '}'
-    .substring(0, stringStartingAtMatch.indexOf('}') + 1) // e.g. { class: "text-[16px]" }
-  // count '{' occurrences (x) and find the x-th '}'
-  const bracketOpenCount = matchPre.match(/\{/g).length
-  const bracketCloseCount = stringStartingAtMatch.matchAll(/\}/g)
-
-  if (bracketOpenCount > 1) {
-    let i = -1
-    for (const bracketClose of bracketCloseCount) {
-      i++
-      if (i === bracketOpenCount - 1) {
-        // console.log(`Found ${bracketClose[0]} index=${bracketClose.index}`)
-        matchPre = stringStartingAtMatch
-          .substring(0, bracketClose.index + 1)
+    if (bracketOpenCount > 1) {
+      let i = -1
+      for (const bracketClose of bracketCloseCount) {
+        i++
+        // find matching closing bracket 
+        if (i === bracketOpenCount - 1) {
+          // e.g. '{ name: { first: { callMe: 'steven' } } }'
+          matchPre = stringStartingAtMatch.substring(0, bracketClose.index + 1)
+        }
       }
     }
+
+    // turn stringified obj into a JSON-praseable obj so we can work with an obj instead of string
+    const match = matchPre
+      .replace(/\s{2,}/g, ' ').trim() // remove all excess whitespace
+      .replace(/(\b\w+\b)(: ")/g, '"$1"$2') // e.g. { class: "text-lg" } -> { "class": "text-lg" }
+      .replace(/style:/g, '"style":') // e.g. style: fontFamily -> "style": fontFamily
+      // put double quotes around the style value,spread obj"{...obj1, ...obj2}"
+      .replace(/"style": \{([^}]*)\}/g, '"style": "{$1}"')
+      // todo: put double quotes around the style value if its a single variable name 
+      .replace(/'/g, '"') // replace single with double quotes (only found in values)
+
+    const obj = inlineTw(JSON.parse(match), cleanTwCss)
+
+    const processObj = (obj: { [key: string]: string }): string => {
+      if (bracketOpenCount > 1) {
+        // The style value is an obj and should not have double quotes around it 
+        const jsonObj = JSON.stringify((obj))
+        const indexOfStyle = jsonObj.indexOf('"style":') + '"style":'.length
+        const substringAfterStyle = jsonObj.substring(indexOfStyle)
+        const modifiedSubstring = substringAfterStyle.replace(/"/, '').replace(/"/, '')
+
+        return `${jsonObj.substring(0, indexOfStyle) + modifiedSubstring}`
+      } else {
+        return `${JSON.stringify(obj)}`
+      }
+    }
+
+    const processedObj = processObj(obj)
+
+    // replace the old obj inside the string
+    const substituteObj = (stringObj: string): string => {
+      return `${code.substring(0, regexStart.lastIndex - 1)
+        + processedObj
+        + code.substring(indexStart + matchPre.length)
+        }`
+    }
+
+    code = substituteObj(processedObj)
   }
 
-  if (bracketCloseCount > 2) {
-    throw new Error('You have passed in a nested object as a style prop. Email component style props cannot be nested.')
-  }
+  code = handleHead(code, cleanTwCss)
 
-  const match = matchPre
-    .replace(/\s{2,}/g, ' ').trim() // remove all excess whitespace
-    .replace(/(\b\w+\b)(: ")/g, '"$1"$2') // e.g. { class: "text-lg" } -> { "class": "text-lg" }
-    .replace(/style:/g, '"style":') // e.g. style: fontFamily -> "style": fontFamily
-    // put double quotes around the style value,spread obj"{...obj1, ...obj2}"
-    .replace(/"style": \{([^}]*)\}/g, '"style": "{$1}"')
-    // todo: put double quotes around the style value if its a single variable name 
-    .replace(/'/g, '"') // replace single with double quotes (only found in values)
-  const obj = JSON.parse(match)
+  fs.writeFileSync(pathToSsrCode, code, 'utf8')
+  const getCompiledEmail = async () => (await import('./welcome-tailwind.js')).default
+  const compiledEmail = await getCompiledEmail()
 
-  // 2.
+  const props: ComponentProps<WelcomeTailwindProps> = { name: { firstName: 'Steven' } }
+  const { html } = compiledEmail.render({ props })
+  return html
+}
 
+function inlineTw(obj: { [k: string]: string }, twClean: string) {
   if (obj.class) {
     // 3. transform tw classes to styles
-    const cssMap = makeCssMap(cleanTailwindCss)
+    const cssMap = makeCssMap(twClean)
     const cleanRegex = /[:#\!\-[\]\/\.%]+/g
 
     // Replace all non-alphanumeric characters with underscores
@@ -124,63 +172,40 @@ while ((matchStart = regexStart.exec(code)) !== null) {
       delete obj.class
     }
   }
+  return obj
+}
 
+function handleHead(code: string, twClean: string) {
+  // 3. Handle responsive head styles
 
-  // replace the old obj inside the string
-  let processedObj: string
-  if (bracketOpenCount > 1) {
-    const jsonObj = JSON.stringify((obj))
-    const indexOfStyle = jsonObj.indexOf('"style":') + '"style":'.length
-    const substringAfterStyle = jsonObj.substring(indexOfStyle)
-    const modifiedSubstring = substringAfterStyle.replace(/"/, '').replace(/"/, '')
-    // console.log(modifiedSubstring)
-    processedObj = jsonObj.substring(0, indexOfStyle) + modifiedSubstring
+  const headStyle = `<style>${getMediaQueryCss(twClean)}</style>`
+  // const hasResponsiveStyles = /@media[^{]+\{(?<content>[\s\S]+?)\}\s*\}/gm.test(headStyle)
+  const startString = '${validate_component(Head, "Head").$$render($$result, {}, {}, {' // change this to find the third {
+  const startIndex = code.indexOf(startString)
+  const stringAfterStart = code.substring(startIndex, code.length)
+  const endIndex = startIndex + stringAfterStart.indexOf('})}')
+  const inbetween = code.slice(startIndex + startString.length, endIndex)
+
+  if (inbetween.includes('default:')) {
+    //3a. take the old string and concat the <style> tag.
+    const headChildStart = startIndex + startString.length + inbetween.indexOf('`') + 1
+    const headChildEnd = startIndex + startString.length + inbetween.lastIndexOf('`')
+    const headChildOld = code.substring(headChildStart, headChildEnd)
+
+    // console.log(code.substring(headChildStart, headChildEnd))
+    return `${code.substring(0, headChildStart - 1)
+      + '`' + headChildOld + headStyle + '`'
+      + code.substring(headChildEnd + 1)
+      }`
   } else {
-    processedObj = JSON.stringify(obj)
+    //3b. If no children already present, Insert: default: () => { return ``; }
+    const headChildStart = startIndex + startString.length
+    const headChildEnd = endIndex
+    const headChild = `default: () => { return '${headStyle}'; }`
+
+    return `${code.substring(0, headChildStart)
+      + headChild
+      + code.substring(headChildEnd)
+      }`
   }
-
-  code =
-    code.substring(0, regexStart.lastIndex - 1) // substring up to match
-    + processedObj // concat new obj string
-    + code.substring(indexStart + matchPre.length) // substring after match
-  if (bracketOpenCount > 1) {
-    console.log(obj)
-    console.log(processedObj)
-    // console.log('matchPre: ', matchPre)
-  }
-  //
 }
-
-// 3. Handle responsive head styles
-const startString = '${validate_component(Head, "Head").$$render($$result, {}, {}, {' // change this to find the third {
-const startIndex = code.indexOf(startString)
-const stringAfterStart = code.substring(startIndex, code.length)
-const endIndex = startIndex + stringAfterStart.indexOf('})}')
-const inbetween = code.slice(startIndex + startString.length, endIndex)
-
-if (inbetween.includes('default:')) {
-  //3a. take the old string and concat the <style> tag.
-  const headChildStart = startIndex + startString.length + inbetween.indexOf('`') + 1
-  const headChildEnd = startIndex + startString.length + inbetween.lastIndexOf('`')
-  const headChildOld = code.substring(headChildStart, headChildEnd)
-
-  // console.log(code.substring(headChildStart, headChildEnd))
-  code =
-    code.substring(0, headChildStart - 1)
-    + '`' + headChildOld + headStyle + '`'
-    + code.substring(headChildEnd + 1)
-} else {
-  //3b. If no children already present, Insert: default: () => { return ``; }
-  const headChildStart = startIndex + startString.length
-  const headChildEnd = endIndex
-  const headChild = `default: () => { return '${headStyle}'; }`
-  code =
-    code.substring(0, headChildStart)
-    + headChild
-    + code.substring(headChildEnd)
-}
-
-fs.writeFileSync(pathToSsrCode, code, 'utf8')
-const getCompiledEmail = async () => (await import('./welcome-tailwind.js')).default
-const compiledEmail = await getCompiledEmail()
-const { html } = compiledEmail.render()
