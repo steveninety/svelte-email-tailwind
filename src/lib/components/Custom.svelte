d675b109f3388311dfdccc6718e358bab36b0404
steveninety <admin@stevenpolak.me>
2024-03-05 19:34:39 +0100

    add custom el component; improved script's reliability of stringified prop manipulation

---
+++ src/lib/components/Custom.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import type {
+		StandardLonghandProperties,
+		StandardProperties,
+		StandardShorthandProperties
+	} from 'csstype';
+	import { styleToString } from '$lib/utils';
+	import type { HTMLAttributes } from 'svelte/elements';
+	interface $$Props extends Omit<HTMLAttributes<HTMLParagraphElement>, 'style'> {
+		style?: StandardLonghandProperties & StandardShorthandProperties & StandardProperties;
+	}
+
+	export let style: $$Props['style'] = {};
+	export let styleString: string = '';
+	export let as = 'div';
+	let className: string | undefined = undefined;
+	export { className as class };
+
+	const styleDefault = styleToString({
+		...style
+	});
+	// console.log(styleDefault);
+</script>
+
+<svelte:element this={as} style={styleDefault + styleString} class={className} {...$$restProps}>
+	<slot />
+</svelte:element>
